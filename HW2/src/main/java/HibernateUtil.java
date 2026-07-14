48f1dd3633c53e17107c3b4a63225363f759dda1
asiostyx-create <asio.styx@gmail.com>
2026-07-14 20:18:44 +0300

    added exceptions (i forgor)

--- HW2/src/main/java/HibernateUtil.java
+++ HW2/src/main/java/HibernateUtil.java
@@ -2,22 +2,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
-//public class HibernateUtil {
-//    private static final SessionFactory sessionFactory = buildSessionFactory();
-//
-//    private static SessionFactory buildSessionFactory() {
-//        try {
-//            return new Configuration().configure().buildSessionFactory();
-//        } catch (Throwable ex) {
-//            System.err.println("Ошибка: " + ex);
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-//
-//    public static SessionFactory getSessionFactory() {
-//        return sessionFactory;
-//    }
-
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
@@ -32,7 +16,7 @@ public class HibernateUtil {
                 sessionFactory = configuration.buildSessionFactory(builder.build());
 
             } catch (Exception e) {
-                System.out.println("Исключение " + e);
+                System.out.println("Ошибка в фабрике сессий: " + e.getMessage());
             }
         }
         return sessionFactory;
--- HW2/src/main/java/UsersDaoImpl.java
+++ HW2/src/main/java/UsersDaoImpl.java
@@ -7,40 +7,53 @@ import java.util.Optional;
 public class UsersDaoImpl implements UsersDao {
     @Override
     public void save(Users users) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tr = session.beginTransaction();
-        session.save(users);
-        tr.commit();
-        session.close();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tr = session.beginTransaction();
+            session.save(users);
+            tr.commit();
+        } catch (Exception e) {
+            System.out.ptintln(e.getMessage());
+        }
     }
 
     @Override
     public void update(Users users) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tr = session.beginTransaction();
-        session.update(users);
-        tr.commit();
-        session.close();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tr = session.beginTransaction();
+            session.update(users);
+            tr.commit();
+        } catch (Exception e) {
+            System.out.ptintln(e.getMessage());
+        }
     }
 
     @Override
     public Users findById(int id) {
-        return HibernateUtil.getSessionFactory().openSession().get(Users.class, id);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Users.class, id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public List<Users> findAll() {
-        List<Users> usersAll = (List<Users>) HibernateUtil.getSessionFactory().openSession().createQuery("From Users").list();
-        return usersAll;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("From Users").list();
+        } catch (Exception e) {
+            System.out.ptintln(e.getMessage());
+            return List.of();
     }
 
     @Override
     public void delete(Users users) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tr = session.beginTransaction();
-        session.delete(users);
-        tr.commit();
-        session.close();
-
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction tr = session.beginTransaction();
+            session.delete(users);
+            tr.commit();
+        } catch (Exception e) {
+            System.out.ptintln(e.getMessage());
+        }
     }
 }
